9c0039d2c96e0a3ae3670204e1d6f9e6dd2b2d39
mkg880 <mehrzadgandhi@gmail.com>
2024-10-24 14:48:54 -0400

    added solutions

---
+++ Solution_230.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    int sol = 0;
+    public void traverse(TreeNode root, int k) {
+        if(root == null) return;
+        traverse(root.left, k);
+        count++;
+        if(count == k) sol = root.val;
+        traverse(root.right, k);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k);
+        return sol;
+    }
+}
