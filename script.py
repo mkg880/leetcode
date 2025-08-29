693009443654d5cfd4fcebd81e1721e903ea48eb
mkg880 <mehrzadgandhi@gmail.com>
2025-08-29 15:17:01 -0700

    updated script

--- script.py
+++ script.py
@@ -1,9 +1,21 @@
 import time
+from git import Repo
+num = None
+file_name = None
+repo_path = '/Users/mkg/Downloads/leetcode'
+repo = Repo(repo_path)
 while True:
     s = input()
-    arr = s.split()
-    if len(arr) <= 1:
-        continue
-    arr[0] = arr[0][:-1]
-    time.sleep(0.05)
-    print("_".join(arr) + ".py")
+    if s.lower() == 'done':
+        repo.index.add([file_name])
+        repo.index.commit(f"added {num}")
+        # repo.remotes[0].push()
+        current_branch = repo.active_branch.name
+        repo.remotes.origin.push(refspec=f"HEAD:refs/heads/{current_branch}")
+    else:
+        arr = s.split()
+        arr[0] = arr[0][:-1]
+        num = arr[0]
+        time.sleep(0.05)
+        file_name = "_".join(arr) + ".py"
+        print(file_name)
