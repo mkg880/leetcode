2af483d0832a895ce8b163866e56b0dd5282d1cc
mkg880 <mehrzadgandhi@gmail.com>
2025-09-12 16:42:19 -0700

    added 3137

---
+++ 3137_Minimum_Number_of_Operations_to_Make_Word_K-Periodic.py
@@ -0,0 +1,6 @@
+from typing import Counter
+class Solution:
+    def minimumOperationsToMakeKPeriodic(self, word: str, k: int) -> int:
+        n = len(word)
+        c = Counter([word[i:i+k] for i in range(0, n, k)])
+        return n // k - max(c.values())
