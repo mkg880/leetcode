afd8aa5e35092040e7cab981e50821b6214e5e1b
mkg880 <mehrzadgandhi@gmail.com>
2025-09-16 16:29:48 -0700

    added 3223

--- 3223_Minimum_Length_of_String_After_Operations.py
+++ 3223_Minimum_Length_of_String_After_Operations.py
@@ -1,14 +1,4 @@
+from typing import Counter
 class Solution:
     def minimumLength(self, s: str) -> int:
-        cnt = {}
-        for c in s:
-            if c not in cnt:
-                cnt[c] = 0
-            cnt[c] += 1
-        res = 0
-        for key in cnt:
-            if cnt[key] % 2 == 0:
-                res += 2
-            else:
-                res += 1
-        return res
+        return sum(2 if val % 2 == 0 else 1 for val in Counter(s).values())
