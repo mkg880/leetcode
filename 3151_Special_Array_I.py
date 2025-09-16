a9ecc62c8bfc0d427322909dafd1ddeb5be1b60b
mkg880 <mehrzadgandhi@gmail.com>
2025-09-16 13:19:46 -0700

    added 3151

---
+++ 3151_Special_Array_I.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        parity = nums[0] % 2
+        for i in range(1, len(nums)):
+            if nums[i] % 2 == parity:
+                return False
+            parity = nums[i] % 2
+        return True
